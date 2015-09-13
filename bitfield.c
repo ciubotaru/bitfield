3869be5ea74254fd4e140957dae0c595dfd3f3bd
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2015-09-13 23:03:00 +0900

    Added descriptions to source files

--- bitfield.c
+++ bitfield.c
@@ -1,3 +1,12 @@
+/**
+ * File name: bitfield.c
+ * Project name: bitfield, a bit array manipulation library written in C
+ * URL: https://github.com/ciubotaru/bitfield
+ * Author: Vitalie Ciubotaru <vitalie at ciubotaru dot tk>
+ * License: General Public License, version 3
+ * Date: September 1, 2015
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
