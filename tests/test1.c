44210fe1ab6d44919b1d6d7d29a6c1cca722d799
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2015-09-20 23:09:00 +0900

    Added test for filling arrays with ones

--- tests/test1.c
+++ tests/test1.c
@@ -13,17 +13,26 @@
 #include <time.h>
 #include "bitfield.h"
 
+/* Testing bfsub() function */
+
 int main()
 {
 	srand((unsigned)time(NULL));
 	int i;			//counter
 	int len = 80;
+	char *errmsg;
+	char *msg = "Testing substring extraction and concatenation";
+	char *failed = "[FAIL]";
+	char *passed = "[PASS]";
+	int dots = len - strlen(msg) - 6;	/* 6 is the length of pass/fail string */
+	printf("%s", msg);
+	for (i = 0; i < dots; i++)
+		printf(".");
 	struct bitfield *input = bfnew(len);
 	for (i = 0; i < len; i++)
 		if (rand() % 2)
 			BITSET(input, i);
-	bfprint(input);
-	char *errmsg;
+//	bfprint(input);
 	int point;
 	for (point = 1; point < (len - 1); point++) {
 		struct bitfield *chunk1 = bfsub(input, 0, point);
@@ -31,15 +40,15 @@ int main()
 //            bfprint(chunk1);
 		//          bfprint(chunk2);
 		struct bitfield *output = bfcat(chunk1, chunk2);
-		bfprint(output);
+//		bfprint(output);
 		if (bfcmp(input, output, &errmsg) != 0) {
-			printf("FAILED\n");
+			printf("%s\n", failed);
 			return 1;
 		}
 		bfdel(output);
 		bfdel(chunk1);
 		bfdel(chunk2);
 	}
-	printf("PASSED\n");
+	printf("%s\n", passed);
 	return 0;
 }
--- tests/test4.c
+++ tests/test4.c
@@ -1,10 +1,10 @@
 /**
- * File name: tests/test3.c
+ * File name: tests/test4.c
  * Project name: bitfield, a bit array manipulation library written in C
  * URL: https://github.com/ciubotaru/bitfield
  * Author: Vitalie Ciubotaru <vitalie at ciubotaru dot tk>
  * License: General Public License, version 3
- * Date: September 1, 2015
+ * Date: September 20, 2015
 **/
 
 #include <stdio.h>
@@ -19,30 +19,27 @@ int main()
 	int i;			//counter
 	int len = 80;
 	char *errmsg;
-	char *msg = "Testing binary logic";
+	char *msg = "Testing bit setting";
 	char *failed = "[FAIL]";
 	char *passed = "[PASS]";
 	int dots = len - strlen(msg) - 6;	/* 6 is the length of pass/fail string */
 	printf("%s", msg);
 	for (i = 0; i < dots; i++)
 		printf(".");
-	struct bitfield *input1 = bfnew(len);
-	struct bitfield *input2 = bfnew(len);
-	struct bitfield *output1 = bfnew(len);
-	struct bitfield *output2 = bfnew(len);
-	for (i = 0; i < len; i++) {
-		if (rand() % 2)
-			BITSET(input1, i);
-		if (rand() % 2)
-			BITSET(input2, i);
-	}
-//	bfprint(input1);
-//	bfprint(input2);
-	output1 = bfxor(input1, input2);
+	char *input_char = "11111111111111111111111111111111111111111111111111111111111111111111111111111111"; /* 80 units */
+	struct bitfield *output1 = bfnew_quick(len);
+	char2bf(input_char, output1);
 //	bfprint(output1);
-	output2 = bfand(bfor(input1, input2), bfnot(bfand(input1, input2)));
+	struct bitfield *output2 = bfnew_ones(len);
 //	bfprint(output2);
+	struct bitfield *output3 = bfnew_quick(len);
+	bfone(output3);
+//	bfprint(output3);
 	if (bfcmp(output1, output2, &errmsg) != 0) {
+		printf("%s\n", failed);
+		return 1;
+	}
+	else if (bfcmp(output1, output3, &errmsg) != 0) {
 		printf("%s\n", failed);
 		return 1;
 	}
