8b9fbd6501028858ea1f07b8b96629df3dc12e31
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2015-10-25 23:03:00 +0900

    Added test for bfrev(). Test fails.

---
+++ tests/test7.c
@@ -0,0 +1,69 @@
+/**
+ * File name: tests/test5.c
+ * Project name: bitfield, a bit array manipulation library written in C
+ * URL: https://github.com/ciubotaru/bitfield
+ * Author: Vitalie Ciubotaru <vitalie at ciubotaru dot tk>
+ * License: General Public License, version 3
+ * Date: October 1, 2015
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bitfield.h"
+#include "bitfield-internals.h"
+
+/* Testing bfresize() */
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int i;			//counter
+	int len = 80;
+	char *msg = "Testing bfresize()";
+	char *failed = "[FAIL]";
+	char *passed = "[PASS]";
+	int dots = len - strlen(msg) - 6;	/* 6 is the length of pass/fail string */
+	printf("%s", msg);
+	for (i = 0; i < dots; i++)
+		printf(".");
+	char *input_char = malloc((len + 1) * sizeof(char));	/* allocate space for 80+1 chars */
+	for (i = 0; i < len; i++) {
+		if (rand() % 2)
+			input_char[i] = '1';
+		else
+			input_char[i] = '0';
+	}
+	input_char[len] = '\0';
+	char *input_char_rev = malloc((len + 1) * sizeof(char));
+	for (i = 0; i < len; i++) {
+		if (input_char[i] == '1')
+			input_char_rev[len - i - 1] = '1';
+		else
+			input_char_rev[len - i - 1] = '0';
+	}
+	input_char_rev[len] = '\0';
+	
+//	printf("%s\n", input_char);
+//	printf("%s\n", input_char_rev);
+
+	struct bitfield *input1 = bfnew_quick(len);
+	char2bf(input_char, input1);
+//	bfprint(input1);
+
+	struct bitfield *input2	= bfrev(input1);
+//	bfprint(input2);
+	for (i = 0; i < len; i++) {
+		if (BITGET(input2, i) != input_char_rev[i] - '0') {
+			printf("%s\n", failed);
+			return 1;
+		}
+	}
+
+	bfrev_ip(input1);
+	bfprint(input1);
+
+	printf("%s\n", passed);
+	return 0;
+}
