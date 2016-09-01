275ead89af621ff1a6273c20c97270d73683a635
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2016-09-01 23:04:00 +0900

    Added test for short2bf()

---
+++ tests/test27.c
@@ -0,0 +1,53 @@
+/**
+ * File name: tests/test27.c
+ * Project name: bitfield, a bit array manipulation library written in C
+ * URL: https://github.com/ciubotaru/bitfield
+ * Author: Vitalie Ciubotaru <vitalie at ciubotaru dot tk>
+ * License: General Public License, version 3 or later
+ * Date: September 1, 2016
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bitfield.h"
+#include "bitfield-internals.h"
+
+/* Testing short2bf() */
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int i, j;		//counters
+	int len = 80;
+	char *msg = "Testing short2bf()";
+	char *failed = "[FAIL]";
+	char *passed = "[PASS]";
+	int dots = len - strlen(msg) - 6;	/* 6 is the length of pass/fail string */
+	printf("%s", msg);
+	for (i = 0; i < dots; i++)
+		printf(".");
+	int bitnslots = (len - 1) / SHORT_BIT + 1;
+	unsigned short *input = calloc(1, bitnslots * sizeof(unsigned short));
+	for (i = 0; i < bitnslots - 1; i++) {
+		for (j = 0; j < SHORT_BIT; j++) {
+			if (rand() % 2)
+				input[i] |= (1U << j);
+		}
+	}
+	for (i = 0; i < len % SHORT_BIT; i++)
+		if (rand() % 2)
+			input[bitnslots - 1] |= (1U << i);
+	struct bitfield *output = short2bf(input, len);
+	for (i = 0; i < bitnslots; i++) {
+		if ((unsigned short)
+		    bfsub(output, i * SHORT_BIT,
+			  (i + 1) * SHORT_BIT)->field[0] != input[i]) {
+			printf("%s\n", failed);
+			return 1;
+		}
+	}
+	printf("%s\n", passed);
+	return 0;
+}
